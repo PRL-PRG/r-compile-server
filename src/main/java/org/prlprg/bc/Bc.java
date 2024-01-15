2aa812b26b76e4f97f0678518f4462a8a5e2aabe
jakobeha <jakobeha@gmail.com>
2024-01-14 19:29:43 -0500

    remove renjin dependency

--- src/main/java/org/prlprg/bc/Bc.java
+++ src/main/java/org/prlprg/bc/Bc.java
@@ -1,10 +1,12 @@
 package org.prlprg.bc;
 
 import com.google.common.collect.ImmutableList;
-import org.renjin.sexp.SEXP;
+
+// TODO: Remove stub with Sexp from branch `filip`
+class Sexp {}
 
 /** A complete R bytecode, consisting of a version, array of instructions and associated data, and constants. */
-public record Bc(BcCode code, ImmutableList<SEXP> constants) {
+public record Bc(BcCode code, ImmutableList<Sexp> constants) {
     /**
      * The only version of R bytecodes we support, which is also the latest version.
      * The bytecode's version is denoted by the first integer in its code.
