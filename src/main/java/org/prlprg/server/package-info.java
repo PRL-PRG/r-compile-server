1a1f31ef364043313b92c201ecc052723f775b18
jakobeha <jakobeha@gmail.com>
2024-02-03 20:34:01 -0500

    add package-info

---
+++ src/main/java/org/prlprg/server/package-info.java
@@ -0,0 +1,9 @@
+/** Socket setup and communication with clients, and protocols. */
+@ParametersAreNonnullByDefault
+@FieldsAreNonNullByDefault
+@ReturnTypesAreNonNullByDefault
+package org.prlprg.server;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import org.prlprg.util.FieldsAreNonNullByDefault;
+import org.prlprg.util.ReturnTypesAreNonNullByDefault;
