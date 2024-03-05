5d4135c19a3f459491fb94a2ca94ab6ff2b8539a
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2024-03-05 17:52:32 +0100

    Minimal compiling RDSWriter

--- src/main/java/org/prlprg/rds/RDSWriter.java
+++ src/main/java/org/prlprg/rds/RDSWriter.java
@@ -1,6 +1,21 @@
 package org.prlprg.rds;
 
 import java.io.*;
+
+import org.prlprg.RSession;
 import org.prlprg.sexp.*;
 
-public class RDSWriter implements Closeable {}
+public class RDSWriter implements Closeable {
+    private final RSession session;
+    private final RDSOutputStream out;
+
+    private RDSWriter(RSession session, OutputStream out) {
+        this.session = session;
+        this.out = new RDSOutputStream(out);
+    }
+
+    @Override
+    public void close() throws IOException {
+        out.close();
+    }
+}
