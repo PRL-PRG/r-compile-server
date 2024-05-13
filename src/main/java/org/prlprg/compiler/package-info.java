c0a2105982c4e151338cc256903c4cdc41399f12
jakobeha <jakobeha@gmail.com>
2024-05-13 14:45:06 -0400

    add compiler package

---
+++ src/main/java/org/prlprg/compiler/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Compile {@link org.prlprg.sexp.CloSXP} into {@link org.prlprg.ir.closure.Closure}, and {@link
+ * org.prlprg.bc.Bc} into {@link org.prlprg.ir.cfg.CFG}.
+ */
+@ParametersAreNonnullByDefault
+@FieldsAreNonNullByDefault
+@ReturnTypesAreNonNullByDefault
+package org.prlprg.compiler;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import org.prlprg.util.FieldsAreNonNullByDefault;
+import org.prlprg.util.ReturnTypesAreNonNullByDefault;
