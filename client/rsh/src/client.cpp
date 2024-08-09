f2d3ed937827e0d158c98764f9ad57b634c118ea
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2024-08-09 16:27:42 +0200

    Start of gRPC in the client

--- client/rsh/src/client.cpp
+++ client/rsh/src/client.cpp
@@ -1,5 +1,6 @@
 #include "client.h"
-#include "protocol.pb.h"
+#include "messages.pb.h"
+#include "routes.pb.h"
 #include <iostream>
 
 namespace rsh {
@@ -39,4 +40,44 @@ CompileResponse remote_compile(std::string const &name,
 
   return response;
 }
+
+class Client {
+private:
+  std::unique_ptr<RouteGuide::Stub> stub_;
+
+public:
+  Client(std::shared_ptr<Channel> channel) : stub_(Routes::NewStub(channel)) {
+
+    // TODO: send handshake here?
+  }
+
+  CompileResponse remote_compile(std::string const& name, 
+                                std::vector<uint8_t> const &rds_closure,
+                                Tier tier,
+                                int32_t optimization_level) {
+
+    CompileRequest request;
+    request.set_tier(tier);
+    request.set_optimization_level(optimization_level);
+    request.mutable_function().set_name(name);
+    request.mutable_function().set_body(rds_closure.data(), rds_closure.size());
+
+    // TODO: Actually compile the hash
+    std::array<uint8_t,64> hash = {0};
+    request.mutable_function().set_hash(hash.data(), hash.size());
+
+    ClientContext context;
+    CompileResponse response;
+    Status status = _stub->Compile(&context, request, &response);
+    if(!status.ok()) {
+      std::err << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      exit(1);//TODO: rather return an optional?
+    }
+    else {
+      return response;
+    }
+  }
+};
+
 } // namespace rsh
