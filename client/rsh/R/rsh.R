0f2dae0292d5e48e668b065c4fbe52e7b8b272fc
Filip Krikava <krikava@gmail.com>
2024-09-14 20:32:15 +0000

    Fix test

--- client/rsh/R/rsh.R
+++ client/rsh/R/rsh.R
@@ -67,6 +67,10 @@ rsh_compile <- function(f, options) {
     options$name <- as.character(substitute(f))
   }
 
+  if (is.null(options$inplace)) {
+    options$inplace <- TRUE
+  }
+
   invisible(.Call(C_compile, f, options))
 }
 
--- client/rsh/tests/test-remote-compilation.R
+++ client/rsh/tests/test-remote-compilation.R
@@ -1,7 +1,7 @@
-# f <- function() {
-#   42
-# }
-#
-# rsh::rsh_compile(f)
-# stopifnot(rsh::is_compiled(f))
-# stopifnot(f() == 42)
+f <- function() {
+  42
+}
+
+rsh::rsh_compile(f)
+stopifnot(rsh::is_compiled(f))
+stopifnot(f() == 42)
