ea60a4ca367498c2aa24302694aaf8b95bbcc633
vinho <vholuni@gmail.com>
2025-07-14 12:44:26 +0000

    More comprehensive DollarGets test

--- server/src/test/java/org/prlprg/bc2c/BC2CCompilerTest.java
+++ server/src/test/java/org/prlprg/bc2c/BC2CCompilerTest.java
@@ -655,17 +655,30 @@ public class BC2CCompilerTest {
   public void testDollarGets(BC2CSnapshot snapshot) {
     snapshot.verify(
         """
-            x <- list(a = 1, b = 2)
-            class(x) <- "myobject"
+  # Basic list assignment
+  x1 <- x2 <- list(a = 1)
+  x1$b <- 2
+  eval(quote(x2$b <- 2))
 
-            `$<-.myobject` <- function(x, name, value) {
-              x[[name]] <- value * 100
-              x
-            }
+  # Method dispatch
+  obj <- structure(list(val = 1), class = "myobj")
+  `$<-.myobj` <- function(x, name, value) {
+    x[[paste0("modified_", name)]] <- value
+    x
+  }
+  obj1 <- obj2 <- obj
+  obj1$test <- 10
+  eval(quote(obj2$test <- 10))
 
-            x$a <- 42
-            x
-            """);
+  # Copy-on-write
+  original <- list(a = 1)
+  x3 <- x4 <- original
+  x3$b <- 2
+  eval(quote(x4$b <- 2))
+
+  identical(x1, x2) && identical(obj1, obj2) && !identical(x3, original) && !identical(x4, original) && identical(x3, x4)
+            """,
+        returns(SEXPs.TRUE));
   }
 
   // TODO: test for errors - and stack pointers
