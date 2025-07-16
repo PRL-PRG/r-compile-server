ed45625190428805930a5b9379d6cf0a39a4b1c9
Pierre DOnat-Bouillud <pierre.donat.bouillud@gmail.com>
2025-07-16 16:51:16 +0000

    Collect bytecode profiles.

---
+++ client/rsh/inst/benchmarks/collect-bc-profiles.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env Rscript
+set.seed(1)
+
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(stringr))    
+suppressPackageStartupMessages(library(purrr))
+
+# Function to load all CSV files in a folder into tibbles
+load_csvs_into_tibbles <- function(folder_path) {
+  if (!dir.exists(folder_path)) {
+    stop("The specified folder does not exist.")
+  }
+
+  csv_files <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
+  # remove merged_output.csv if it exists
+  csv_files <- csv_files[!basename(csv_files) %in% "merged_output.csv"]
+
+  if (length(csv_files) == 0) {
+    stop("No CSV files found in the specified folder.")
+  }
+
+  tibble_list <- map(csv_files, ~ read_csv(.x, show_col_types = FALSE))
+
+
+  return(tibble_list)
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) == 0) {
+  stop("Please provide a folder path as a command-line argument.")
+}
+
+folder_path <- args[1]
+
+tibbles <- load_csvs_into_tibbles(folder_path)
+
+df <- bind_rows(tibbles) |>
+    mutate(across( # Replace NA with 0 for all uppercase columns, which happen to be all the opcodes
+    all_of(names(df)[str_detect(names(df), "^[A-Z0-9_]+$")]),
+    ~ replace_na(., 0)
+  ))
+
+
+  # save to CSV 
+output_file <- file.path(folder_path, "merged_output.csv")
+write_csv(df, output_file)
