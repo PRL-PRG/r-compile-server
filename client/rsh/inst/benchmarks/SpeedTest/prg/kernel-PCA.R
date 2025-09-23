0959cdbad34d4490352bd9d40d83a5ead637d9f4
Filip Krikava <krikava@gmail.com>
2025-09-23 13:20:25 +0000

    Enable kernel-PCA benchmark

--- client/rsh/inst/benchmarks/SpeedTest/prg/kernel-PCA.R
+++ client/rsh/inst/benchmarks/SpeedTest/prg/kernel-PCA.R
@@ -1,49 +1,49 @@
-# execute <- function(size = 100L) {
-#   kpca <- function(Xtrn, Xtst, rho, m) {
-#     n <- nrow(Xtrn)
-#
-#     K <- matrix(NA, n, n)
-#     for (i in 1:n) {
-#       for (j in i:n) {
-#         K[i,j] <- K[j,i] <- exp(-rho^2*sum((Xtrn[i,]-Xtrn[j,])^2))
-#       }
-#     }
-#
-#     M <- diag(n) - matrix(1/n, n, n)
-#     e <- eigen(M %*% K %*% M, symmetric=TRUE)
-#
-#     prj <- matrix(NA, nrow(Xtst), m)
-#     Ks <- colSums(K)/n
-#     for (t in 1:nrow(Xtst)) {
-#       k <- rep(NA, n)
-#       for (i in 1:n) {k[i] <- exp(-rho^2*sum((Xtrn[i,]-Xtst[t,])^2))}
-#       for (w in 1:m) {prj[t,w] <- (k - Ks) %*% M %*% e$vectors[,w] / sqrt(e$values[w])}
-#     }
-#     prj
-#   }
-#
-#   set.seed(1)
-#   X <- matrix(NA, size, 2)
-#   class <- rep(NA,size)
-#
-#   for (i in 1:(size)) {
-#     theta <- runif(1,0,2*pi)
-#     r <- runif(1,1,1.75)
-#     X[i,1] <- r*cos(theta)
-#     X[i,2] <- 1.1*r*sin(theta)
-#
-#     class[i] <- as.numeric(runif(1)<0.5)
-#     if (class[i])
-#     { X[i,1] <- 0.8*X[i,1]+0.1
-#       X[i,2] <- 0.7*X[i,2]-0.2
-#     }
-#   }
-#
-#   class.trn <- class[1:size]
-#   class.tst <- class[(size+1):(size)]
-#
-#   for (i in 1:25) {prj <- kpca(X, X, 1, 3)}
-#   res <- coef(glm(class~prj,family="binomial"))
-#
-#   return(res)
-# }
+execute <- function(size = 100L) {
+  kpca <- function(Xtrn, Xtst, rho, m) {
+    n <- nrow(Xtrn)
+
+    K <- matrix(NA, n, n)
+    for (i in 1:n) {
+      for (j in i:n) {
+        K[i,j] <- K[j,i] <- exp(-rho^2*sum((Xtrn[i,]-Xtrn[j,])^2))
+      }
+    }
+
+    M <- diag(n) - matrix(1/n, n, n)
+    e <- eigen(M %*% K %*% M, symmetric=TRUE)
+
+    prj <- matrix(NA, nrow(Xtst), m)
+    Ks <- colSums(K)/n
+    for (t in 1:nrow(Xtst)) {
+      k <- rep(NA, n)
+      for (i in 1:n) {k[i] <- exp(-rho^2*sum((Xtrn[i,]-Xtst[t,])^2))}
+      for (w in 1:m) {prj[t,w] <- (k - Ks) %*% M %*% e$vectors[,w] / sqrt(e$values[w])}
+    }
+    prj
+  }
+
+  set.seed(1)
+  X <- matrix(NA, size, 2)
+  class <- rep(NA,size)
+
+  for (i in 1:(size)) {
+    theta <- runif(1,0,2*pi)
+    r <- runif(1,1,1.75)
+    X[i,1] <- r*cos(theta)
+    X[i,2] <- 1.1*r*sin(theta)
+
+    class[i] <- as.numeric(runif(1)<0.5)
+    if (class[i])
+    { X[i,1] <- 0.8*X[i,1]+0.1
+      X[i,2] <- 0.7*X[i,2]-0.2
+    }
+  }
+
+  class.trn <- class[1:size]
+  class.tst <- class[(size+1):(size)]
+
+  for (i in 1:25) {prj <- kpca(X, X, 1, 3)}
+  res <- coef(glm(class~prj,family="binomial"))
+
+  return(res)
+}
