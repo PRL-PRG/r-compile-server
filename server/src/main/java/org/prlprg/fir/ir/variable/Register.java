04c2b77b38269a5d41f430e2509ccec4380e681c
jakobeha <jakobeha@fastmail.com>
2026-03-04 15:02:35 +0100

    @WIP name improvements

--- server/src/main/java/org/prlprg/fir/ir/variable/Register.java
+++ server/src/main/java/org/prlprg/fir/ir/variable/Register.java
@@ -1,5 +1,7 @@
 package org.prlprg.fir.ir.variable;
 
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
 import java.util.Objects;
 import org.prlprg.parseprint.ParseMethod;
 import org.prlprg.parseprint.Parser;
@@ -8,9 +10,45 @@ import org.prlprg.parseprint.Printer;
 public final class Register implements Variable {
   public static final String DEFAULT_NAME = "r";
 
+  private static final ImmutableMap<String, String> BUILTIN_RESEMBLANCES =
+      ImmutableMap.ofEntries(
+          Map.entry("...", "ddd"),
+          Map.entry("+", "plus"),
+          Map.entry("-", "minus"),
+          Map.entry("*", "mul"),
+          Map.entry("/", "div"),
+          Map.entry("^", "pow"),
+          Map.entry("%", "mod"),
+          Map.entry("!", "not"),
+          Map.entry("~", "tilde"),
+          Map.entry("?", "question"),
+          Map.entry("&&", "and"),
+          Map.entry("||", "or"),
+          Map.entry("==", "eq"),
+          Map.entry("!=", "neq"),
+          Map.entry("<", "lt"),
+          Map.entry(">", "gt"),
+          Map.entry("<=", "le"),
+          Map.entry(">=", "ge"),
+          Map.entry(":", "colon"),
+          Map.entry("<-", "assign"),
+          Map.entry("<<-", "superassign"),
+          Map.entry("[", "extract1"),
+          Map.entry("[[", "extract2"),
+          Map.entry("[<-", "subassign1"),
+          Map.entry("[[<-", "subassign2"),
+          Map.entry("$", "dollar"),
+          Map.entry("$<-", "dollarassign"),
+          Map.entry("@", "at"));
+
   /// Returns a [Register] which resembles `name` but syntactically valid.
   public static Register resemblance(String name) {
-    var base = name.equals("...") ? "ddd" : name.replaceAll("[^a-zA-Z0-9_]", "_");
+    var builtin = BUILTIN_RESEMBLANCES.get(name);
+    if (builtin != null) {
+      return Variable.register(builtin);
+    }
+
+    var base = name.replaceAll("[^a-zA-Z0-9_]", "_");
     if ((base.charAt(0) >= '0' && base.charAt(0) <= '9')
         || base.equals("_")
         || base.startsWith("TRUE")
