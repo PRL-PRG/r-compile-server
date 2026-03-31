e34ba5d2cd0911a40b41794002b9b6147af39cc0
jakobeha <jakobeha@fastmail.com>
2026-03-31 12:51:20 +0200

    @WIP remove specific optimization snapshot tests
    specific optimizations can be tested by unit tests; maintaining the snapshot tests is too much work and running them takes too long

--- server/src/test/java/org/prlprg/snapshot/fir2c/FirAfterOpt2CTest.java
+++ server/src/test/java/org/prlprg/snapshot/fir2c/FirAfterOpt2CTest.java
@@ -1,14 +1,11 @@
 package org.prlprg.snapshot.fir2c;
 
-import org.prlprg.fir.opt.Optimization;
-import org.prlprg.snapshot.fir.opt.OptimizationTestClass;
 import org.prlprg.snapshot.gen2c.CompiledModuleQuery;
 import org.prlprg.snapshot.gen2c.Gen2CCompilerTest;
 
-@OptimizationTestClass
-record FirAfterOpt2CTest(Optimization optimization) implements Gen2CCompilerTest {
+class FirAfterOpt2CTest implements Gen2CCompilerTest {
   @Override
   public CompiledModuleQuery moduleQuery() {
-    return new Fir2CQuery("opt." + optimization.name() + ".fir2c", optimization);
+    return Fir2CQuery.OPTIMIZED_ONLY_FIR;
   }
 }
