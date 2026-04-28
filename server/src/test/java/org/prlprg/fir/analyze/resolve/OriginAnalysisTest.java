8f59eabb98e81a6b1c8d6b465147dcbfc2bc2683
jakobeha <jakobeha@fastmail.com>
2026-04-28 21:20:15 +0200

    new origin analysis tests

--- server/src/test/java/org/prlprg/fir/analyze/resolve/OriginAnalysisTest.java
+++ server/src/test/java/org/prlprg/fir/analyze/resolve/OriginAnalysisTest.java
@@ -10,6 +10,7 @@ import org.prlprg.fir.ir.argument.Constant;
 import org.prlprg.fir.ir.argument.Read;
 import org.prlprg.fir.ir.value.Value;
 import org.prlprg.fir.ir.variable.Variable;
+import org.prlprg.sexp.SEXPs;
 
 class OriginAnalysisTest {
   @Test
@@ -43,12 +44,12 @@ class OriginAnalysisTest {
     var firText =
         """
       fun main() {
-        () --> I { var x:I?, reg r0:I, reg r1:I? |
+        () --> v1(I)? { var x:v1(I)?, reg r0:v1(I), reg r1:v1(I)? |
           mkenv;
-          r0 = 42;
+          r0 = <int 42>;
           st x = r0;
           r1 = ld x;
-          return r0;
+          return r1;
         }
       }
       """;
@@ -63,7 +64,7 @@ class OriginAnalysisTest {
 
     // After store statement, variable x should have r0's origin
     var xOrigin = analysis.get(bb, 2, Variable.named("x"));
-    assertEquals(new Constant(new Value.Int(42)), xOrigin);
+    assertEquals(new Constant(SEXPs.integer(42)), xOrigin);
 
     // After load statement, r1 should have x's origin (the constant)
     var r1Origin = analysis.get(Variable.register("r1"));
@@ -77,7 +78,7 @@ class OriginAnalysisTest {
       fun main() {
         (reg r0:I) --> V { reg r1:I, reg r2:I, reg r3:I, reg r4:I, reg r5:I |
           mkenv;
-              r1 = 42;
+          r1 = 42;
           if r0 then BB1() else BB2();
         BB1():
           r2 = r1;
@@ -107,18 +108,19 @@ class OriginAnalysisTest {
     var firText =
         """
       fun main(cond) {
-        (reg cond:B) --> I { var x:I, reg x1:I |
+        (reg cond:B) --> v1(I) { var x:v1(I)?, reg x1:v1(I)?, reg x2:v1(I) |
           mkenv;
           if cond then BB1() else BB2();
         BB1():
-          st x = 1;
+          st x = <int 1>;
           goto BB3();
         BB2():
-          st x = 2;
+          st x = <int 2>;
           goto BB3();
         BB3():
           x1 = ld x;
-          return x1;
+          x2 = x1 as v1(I);
+          return x2;
         }
       }
       """;
@@ -131,7 +133,87 @@ class OriginAnalysisTest {
 
     assertNull(analysis.get(bb3, -1, Variable.named("x")));
     assertEquals(
-        Set.of(new Constant(new Value.Int(1)), new Constant(new Value.Int(2))),
+        Set.of(new Constant(SEXPs.integer(1)), new Constant(SEXPs.integer(2))),
         analysis.getKnown(bb3, -1, Variable.named("x")));
+  }
+
+  @Test
+  void testNestedEnvironments() {
+    var firText =
+        """
+           fun main() {
+             () -~> I { var x:v1(I)?, var y:v1(I)? |
+               mkenv;
+               st x = <int 1>;
+               st y = <int 2>;
+               mkenv;
+               st x = <int 3>;
+               popenv;
+               popenv;
+               return 42;
+             }
+           }
+           """;
+
+    var module = parseModule(firText);
+    var main = Objects.requireNonNull(module.localFunction(Variable.named("main"))).version(0);
+    var entry = Objects.requireNonNull(main.cfg()).entry();
+
+    var analysis = new OriginAnalysis(main);
+
+    assertEquals(Set.of(), analysis.getKnown(entry, 0, Variable.named("x")));
+    assertEquals(Set.of(), analysis.getKnown(entry, 0, Variable.named("y")));
+
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(1))), analysis.getKnown(entry, 3, Variable.named("x")));
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(2))), analysis.getKnown(entry, 3, Variable.named("y")));
+
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(3))), analysis.getKnown(entry, 4, Variable.named("x")));
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(2))), analysis.getKnown(entry, 4, Variable.named("y")));
+
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(1))), analysis.getKnown(entry, 5, Variable.named("x")));
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(2))), analysis.getKnown(entry, 5, Variable.named("y")));
+
+    assertEquals(Set.of(), analysis.getKnown(entry, 6, Variable.named("x")));
+    assertEquals(Set.of(), analysis.getKnown(entry, 6, Variable.named("y")));
+  }
+
+  @Test
+  void testAmbiguityWithNestedEnvironments() {
+    var firText =
+        """
+           fun main() {
+             (reg r:B) -~> I { var x:v1(I)? |
+               mkenv;
+               st x = <int 1>;
+               mkenv;
+               if b then BB1() else BB2();
+             BB1():
+               st x = <int 2>;
+               goto BB2();
+             BB2():
+               popenv;
+               popenv;
+               return 42;
+             }
+           }
+           """;
+
+    var module = parseModule(firText);
+    var main = Objects.requireNonNull(module.localFunction(Variable.named("main"))).version(0);
+    var bb2 = Objects.requireNonNull(Objects.requireNonNull(main.cfg()).bb("BB2"));
+
+    var analysis = new OriginAnalysis(main);
+
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(1)), new Constant(SEXPs.integer(2))),
+        analysis.getKnown(bb2, -1, Variable.named("x")));
+    assertEquals(
+        Set.of(new Constant(SEXPs.integer(1))), analysis.getKnown(bb2, 0, Variable.named("x")));
   }
 }
